7d7ff5382a5b3ab502a2b707a8856e46295983c6
Matheus Jose <m.theus.jose.pereira@gmail.com>
2025-04-02 14:02:12 -0300

    HeroSection :white_check_mark:

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,11 +3,14 @@ import Image from "next/image";
 import ArroDowm from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from '@/assets/icons/star.svg';
+import SparkleIcon from '@/assets/icons/sparkle.svg'
+
 import { HeroOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="py-32 md:py-48 lg:py-55 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
       <div className="absolute inset-0 -z-30 opacity-5" style={{
         backgroundImage: `url(${grainImage.src})`
       }}></div>
@@ -29,24 +32,46 @@ export const HeroSection = () => {
       <HeroOrbit size={590}rotation={98}>
         <StarIcon className="size-8 text-emerald-300"/>
       </HeroOrbit>
-
+      <HeroOrbit size={430}rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={440}rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={530}rotation={178}>
+        <SparkleIcon className="size-9 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={710}rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={720}rotation={85}>
+        <div className="size-3 rounded-full bg-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={520}rotation={-41}>
+        <div className="size-2 rounded-full bg-emerald-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={650}rotation={-5}>
+        <div className="size-2 rounded-full bg-emerald-300/20"/>
+      </HeroOrbit>
+      </div>
 
 
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={matheusImage} className="size-[150px]" alt="me" />
           <div className="bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="bg-green-500 size-2.5 rounded-full"/>
             <div className=" text-sm font-medium">
               Avaliable for new projects
             </div>
           </div>
         </div>
-        <div className="max-auto max-w-screen-lg">
+        <div className="flex items-center justify-center ">
+        <div className="text-center max-w-lg ">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
             Building expecional user experiences
           </h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">
+          <p className="mt-4 text-center text-white/60 md:text-lg ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -55,6 +80,7 @@ export const HeroSection = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt i
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
+          </div>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
