ebc6891095e2e59a224747a9d8f63fc08d36477f
Matheus Jose <m.theus.jose.pereira@gmail.com>
2025-04-02 12:56:48 -0300

    primeira parte do template HERO

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,64 @@
+import matheusImage from "@/assets/images/perfil-matheus.png";
+import Image from "next/image";
+import ArroDowm from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from '@/assets/icons/star.svg';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 -z-30 opacity-5" style={{
+        backgroundImage: `url(${grainImage.src})`
+      }}></div>
+
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+      <div className="border border-red-500 size-[800px] ">
+      <div className="border border-red-500 inline-flex ">
+        <StarIcon className="size-28 text-emerald-300"/>
+        </div>
+      </div>
+      </div>
+
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image src={matheusImage} className="size-[150px]" alt="me" />
+          <div className="bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className=" text-sm font-medium">
+              Avaliable for new projects
+            </div>
+          </div>
+        </div>
+        <div className="max-auto max-w-screen-lg">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building expecional user experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat null a
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt i
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArroDowm className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+            <span>👋</span>
+            <span className="font-semibold">Lets connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
